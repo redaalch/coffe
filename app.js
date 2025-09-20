6d693da87fe469350180b494e754eb5efe89f98c
redaalch <s133260695@taalim.ma>
2025-09-20 14:01:21 +0100

    starting

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
-  let nav = document.querySelector("nav");
-  nav.innerHTML;
-});
 import Store from "./services/Store.js";
 import API from "./services/API.js";
 
 window.app = {};
 app.store = Store;
+
+window.addEventListener("DOMContentLoaded", () => {
+  let nav = document.querySelector("nav");
+  nav.innerHTML;
+});
