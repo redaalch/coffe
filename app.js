aa8c938a43a1d6a4b79eec68f4c0e51a9f4732c9
redaalch <s133260695@taalim.ma>
2025-09-20 13:49:18 +0100

    starting

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    let nav = document.querySelector("nav");
-})
+window.addEventListener("DOMContentLoaded", () => {
+  let nav = document.querySelector("nav");
+  nav.innerHTML;
+});
+const $ = function(args){ return document.querySelector(args);}
+const $$ = function(args){ return document.querySelectorAll(args);}
+
+HTMLElement.prototype.on = function(a, b, c){ return this.addEventListener(a, b, c); }
+HTMLElement.prototype.off = function(a, b){ return this.removeEventListener(a, b); }
+HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
+HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
