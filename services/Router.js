fd25ea70bf2eab410ff61419c0e29581bd89177e
redaalch <s133260695@taalim.ma>
2025-09-21 18:58:27 +0100

    working on the menu

--- services/Router.js
+++ services/Router.js
@@ -10,21 +10,32 @@ const Router = {
     Router.go(location.pathname);
   },
   go: (route, addToHistory = true) => {
-    if(addToHistory) {
-        history.pushState({route},'',route);
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
     }
-    let pageElmenet=null
+    let pageElmenet = null;
     switch (route) {
-        case "/":
-            pageElmenet = document.createElement("h1");
-            pageElmenet.textContent ="Menu";
-            break;
-        case "/order":
-            pageElmenet = document.createElement("h1");
-            pageElmenet.textContent ="Your order";
-            break;
+      case "/":
+        pageElmenet = document.createElement("h1");
+        pageElmenet.textContent = "Menu";
+        break;
+      case "/order":
+        pageElmenet = document.createElement("h1");
+        pageElmenet.textContent = "Your order";
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElmenet = document.createElement("h1");
+          pageElmenet.textContent = "Details";
+        }
     }
-    document.querySelector("main").appendChild(pageElmenet);
+    if (pageElmenet) {
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElmenet);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
   },
 };
 export default Router;
