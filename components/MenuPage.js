a53a6684b189c9f34b5c309164144892576628f1
redaalch <s133260695@taalim.ma>
2025-09-22 12:02:40 +0100

    working on the menu

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -9,11 +9,37 @@ export class MenuPage extends HTMLElement {
       const css = await request.text();
       styles.textContent = css;
     }
+    loadCSS();
   }
   connectedCallback() {
     const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
     this.root.appendChild(content);
+    window.addEventListener("appmenuchange", () => {
+      this.render();
+    });
+  }
+  render() {
+    if (app.store.menu) {
+      this.root.querySelector("#menu").innerHTML = "";
+      for (let category of app.store.menu) {
+        const liCategory = document.createElement("li");
+        liCategory.innerHTML = `
+        <h3>${category.name}</h3>
+        <ul class='category'>
+
+        </ul>`;
+        this.root.querySelector("#menu").appendChild(liCategory);
+
+        category.products.forEach((product) => {
+          const item = document.createElement("product-item");
+          item.dataset.product = JSON.stringify(product);
+          liCategory.querySelector("ul").appendChild(item);
+        });
+      }
+    } else {
+      this.root.querySelector("#menu").innerHTML = "Loading...";
+    }
   }
 }
 customElements.define("menu-page", MenuPage);
--- services/Menu.js
+++ services/Menu.js
@@ -1,4 +1,5 @@
 import API from "./API.js";
+
 export async function loadData() {
   app.store.menu = await API.fetchMenu();
 }
