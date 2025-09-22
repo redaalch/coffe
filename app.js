f6e2949b8189d11f6932028a4f2773393d897d9c
redaalch <s133260695@taalim.ma>
2025-09-22 13:02:24 +0100

    working on the cart

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ import { MenuPage } from "./components/MenuPage.js";
 import { DetailsPage } from "./components/DetailsPage.js";
 import { OrderPage } from "./components/OrderPage.js";
 import ProductItem from "./components/ProductItem.js";
+import CartItem from "./components/CartItem.js";
+
 window.app = {};
 app.store = Store;
 app.router = Router;
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
   app.router.init();
+});
+window.addEventListener("appcartchange", (event) => {
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quanity, 0);
+  badge.textContent=qty;
+  badge.hidden=qty==0;
 });
