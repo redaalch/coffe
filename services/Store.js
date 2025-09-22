1dde4f7650339b5ffbfb515c2d7ee610311fe96b
redaalch <s133260695@taalim.ma>
2025-09-22 11:39:58 +0100

    working on the menu

--- services/Store.js
+++ services/Store.js
@@ -4,5 +4,17 @@ const Store = {
   menu: null,
   cart: [],
 };
+const proxiedStore = new Proxy(Store, {
+  set(target, property, value) {
+    target[property] = value;
+    if (property == "menu") {
+      window.dispatchEvent(new Event("appmenuchange"));
+    }
+    if (property == "cart") {
+      window.dispatchEvent(new Event("appcartchange"));
+    }
+    return true;
+  },
+});
 
-export default Store;
+export default proxiedStore;
